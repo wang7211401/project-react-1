88f8a09b0416d3454679601c9563949d8f7cfa0f
wang7211401 <wang7211401@163.com>
2018-01-26 09:35:08 +0800

    添加 foo.js

--- dist/bundle.js
+++ dist/bundle.js
@@ -73,6 +73,8 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_lodash___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_lodash__);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_jquery__ = __webpack_require__(4);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_jquery___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_jquery__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__foo__ = __webpack_require__(5);
+
 
 
 
@@ -86,6 +88,9 @@ function component () {
 }
 
 document.body.appendChild(component());
+
+console.log(__WEBPACK_IMPORTED_MODULE_2__foo__["a" /* default */])
+console.log(Object(__WEBPACK_IMPORTED_MODULE_2__foo__["a" /* default */])())
 
 /***/ }),
 /* 1 */
@@ -27604,6 +27609,15 @@ if ( !noGlobal ) {
 return jQuery;
 } );
 
+
+/***/ }),
+/* 5 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony default export */ __webpack_exports__["a"] = (function(){
+   return '方方的教程'
+});
 
 /***/ })
 /******/ ]);
